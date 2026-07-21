b64bd12ab733c038d115a66c4d7503cd2fc68e92
Shine Nyi Nyi Aung <shinenyinyiaung83@gmail.com>
2026-07-21 10:15:12 +0630

    Update database.py

--- database.py
+++ database.py
@@ -2,12 +2,22 @@ import os
 import psycopg2
 from psycopg2.extras import RealDictCursor
 
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
 def connect_db():
-    return psycopg2.connect(
-        os.environ.get("DATABASE_URL"),
-        cursor_factory=RealDictCursor,
-    )
+    if DATABASE_URL:
+        return psycopg2.connect(
+            DATABASE_URL,
+            cursor_factory=RealDictCursor,
+        )
+    else:
+        return psycopg2.connect(
+            host="localhost",
+            database="encrypt_text_web_db",
+            user="postgres",
+            password="12345678",
+            cursor_factory=RealDictCursor,
+        )
 
 
 def init_db():
@@ -26,32 +36,19 @@ def init_db():
 
             cursor.execute(
                 """
-                CREATE TABLE IF NOT EXISTS files (
-                    id SERIAL PRIMARY KEY,
-                    owner_id INTEGER NOT NULL,
-                    file_name TEXT NOT NULL,
-                    encrypted_content TEXT NOT NULL,
-                    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                    UNIQUE(owner_id, file_name),
-                    FOREIGN KEY(owner_id) REFERENCES users(id) ON DELETE CASCADE
-                )
-                """
-            )
-
-            cursor.execute(
-                """
                 CREATE TABLE IF NOT EXISTS received_files (
-                    id SERIAL PRIMARY KEY,
-                    receiver_id INTEGER NOT NULL,
-                    sender_id INTEGER NOT NULL,
-                    file_name TEXT NOT NULL,
-                    encrypted_content TEXT NOT NULL,
-                    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                    UNIQUE(receiver_id, file_name),
-                    FOREIGN KEY(receiver_id) REFERENCES users(id) ON DELETE CASCADE,
-                    FOREIGN KEY(sender_id) REFERENCES users(id) ON DELETE CASCADE
-                )
+                id SERIAL PRIMARY KEY,
+                receiver_id INTEGER NOT NULL,
+                sender_id INTEGER NOT NULL,
+                file_name TEXT NOT NULL,
+                encrypted_content TEXT NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+                UNIQUE(sender_id, receiver_id, file_name),
+
+                FOREIGN KEY(receiver_id) REFERENCES users(id) ON DELETE CASCADE,
+                FOREIGN KEY(sender_id) REFERENCES users(id) ON DELETE CASCADE
+            );
                 """
             )
         conn.commit()
@@ -62,8 +59,13 @@ def execute_query(query, values=()):
     with connect_db() as conn:
         with conn.cursor() as cursor:
             cursor.execute(postgres_query, values)
+
+            row = None
+            if cursor.description:
+                row = cursor.fetchone()
+
             conn.commit()
-            return cursor
+            return row
 
 
 def fetch_one(query, values=()):
@@ -86,66 +88,78 @@ def fetch_user(email):
     return fetch_one("SELECT * FROM users WHERE email = ?", (email,))
 
 
-def fetch_user_by_id(user_id):
-    return fetch_one("SELECT * FROM users WHERE id = ?", (user_id,))
-
-
 def insert_user(data):
     execute_query("INSERT INTO users (email, password_hash) VALUES (?, ?)", data)
 
 
-def insert_file(data):
-    cursor = execute_query(
-        "INSERT INTO files (owner_id, file_name, encrypted_content) VALUES (?, ?, ?) RETURNING id",
-        data,
-    )
-    row = cursor.fetchone()
-    return row["id"] if row else None
+def get_contacts(user_id):
+    with connect_db() as conn:
+        with conn.cursor() as cur:
+            cur.execute("""
+                SELECT DISTINCT
+                    u.id,
+                    u.email
+                FROM users u
+                JOIN (
+                    SELECT sender_id AS contact_id
+                    FROM received_files
+                    WHERE receiver_id = %s
 
+                    UNION
 
-def get_user_files(user_id):
-    return fetch_all("SELECT * FROM files WHERE owner_id = ? ORDER BY id DESC", (user_id,))
+                    SELECT receiver_id AS contact_id
+                    FROM received_files
+                    WHERE sender_id = %s
+                ) c
+                ON u.id = c.contact_id
+                ORDER BY u.email;
+            """, (user_id, user_id))
 
-
-def fetch_file_by_id(file_id, user_id=None):
-    if user_id is None:
-        return fetch_one("SELECT * FROM files WHERE id = ?", (file_id,))
-    return fetch_one("SELECT * FROM files WHERE id = ? AND owner_id = ?", (file_id, user_id))
-
-
-def fetch_received_file_by_id(file_id, user_id=None):
-    if user_id is None:
-        return fetch_one("SELECT * FROM received_files WHERE id = ?", (file_id,))
-    return fetch_one("SELECT * FROM received_files WHERE id = ? AND receiver_id = ?", (file_id, user_id))
-
+            return cur.fetchall()
 
 def insert_received_file(data):
-    cursor = execute_query(
+    row = execute_query(
         "INSERT INTO received_files (receiver_id, sender_id, file_name, encrypted_content) VALUES (?, ?, ?, ?) RETURNING id",
         data,
     )
-    row = cursor.fetchone()
     return row["id"] if row else None
 
 
-def get_received_files(user_id):
-    return fetch_all("SELECT * FROM received_files WHERE receiver_id = ? ORDER BY id DESC", (user_id,))
+def get_received_files(user_id, sender_id=None):
+    if sender_id is None:
+        return fetch_all("SELECT * FROM received_files WHERE receiver_id = ? ORDER BY id DESC", (user_id,))
+    return fetch_all(
+        "SELECT * FROM received_files WHERE receiver_id = ? AND sender_id = ? ORDER BY id DESC",
+        (user_id, sender_id),
+    )
 
 
-def get_decrypted_file_content(file_id, user_id):
-    file_row = fetch_file_by_id(file_id, user_id)
+def get_sent_files(user_id, receiver_id=None):
+    if receiver_id is None:
+        return fetch_all("SELECT * FROM received_files WHERE sender_id = ? ORDER BY id DESC", (user_id,))
+    return fetch_all(
+        "SELECT * FROM received_files WHERE sender_id = ? AND receiver_id = ? ORDER BY id DESC",
+        (user_id, receiver_id),
+    )
 
-    if file_row:
-        return file_row["encrypted_content"]
-    return None
+def all_files(user_id):
+    return fetch_all("SELECT * FROM received_files WHERE receiver_id = ? OR sender_id = ? ORDER BY id DESC", (user_id, user_id))
 
 
-def delete_save_file(file_id, user_id):
-    execute_query("DELETE FROM files WHERE id = ? AND owner_id = ?", (file_id, user_id))
+def delete_file(file_id):
+    execute_query("DELETE FROM received_files WHERE id = ?", (file_id))
 
 
-def delete_transfer_file(file_id, user_id):
-    execute_query("DELETE FROM received_files WHERE id = ? AND receiver_id = ?", (file_id, user_id))
-
+def received_file_exists(sender_id, receiver_id, file_name):
+    return fetch_one(
+        """
+        SELECT id
+        FROM received_files
+        WHERE sender_id = ?
+          AND receiver_id = ?
+          AND file_name = ?
+        """,
+        (sender_id, receiver_id, file_name),
+    )
 
 init_db()
